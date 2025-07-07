84928f16cafa0d64a9241efe5fc76892e0c9e037
Ghellsing007 <garving.vasquez@gmail.com>
2025-07-06 22:52:55 -0400

    new dominio https://gvsanime.netlify.app

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const allowedOrigins = [
   'https://gvsanime.vercel.app', // Frontend en Vercel
   'https://gvsanime-frontend.vercel.app', // Alternativo
   'https://gvsanime.vercel.app', 
-  'https://jolly-tanuki-22b770.netlify.app/'
+  'https://gvsanime.netlify.app'
   // Dominio principal
   process.env.FRONTEND_URL // Variable de entorno para el frontend
 ].filter(Boolean); // Eliminar valores undefined
